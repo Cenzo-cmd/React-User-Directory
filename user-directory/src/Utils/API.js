a0e406287a23f066c2a467af7dc4e43893973f06
Vincent <vrdphone@gmail.com>
2020-12-09 17:56:55 -0600

    Updates

--- user-directory/src/Utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = {
-    getEmployees: function (num = 10) {
-        return axios.get(`https://randomuser.me/api/?results=${num}&nat=us`);
-    }
-}
-
-export default API;
